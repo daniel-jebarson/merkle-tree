c676f2061fcc763c85b454ec7439d5ce82c46928
Dani-Hacker <danieljebarson21@gmail.com>
2022-04-27 19:34:57 +0530

    first commit

---
+++ Dani_Cyber.java
@@ -0,0 +1,37 @@
+package com.example;
+
+public class Dani_Cyber {
+
+	public static void main(String[] args) {
+		
+	String key = "Dani_Cyber";
+    String ori = "My Name is Daniel Jebarson.I lives in Tamil Nadu.";
+    String enc = new Dani_Cyber().encrypt(ori, key);
+    System.out.println(enc);
+    System.out.println(new Dani_Cyber().decrypt(enc, key));
+
+	}
+	public String decrypt(String text, final String key) {
+        String res = "";
+        text = text.toUpperCase();
+        for (int i = 0, j = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c < 'A' || c > 'Z') continue;
+            res += (char)((c - key.charAt(j) + 26) % 26 + 'A');
+            j = ++j % key.length();
+        }
+        return res;
+    }
+	
+	public String encrypt(String text, final String key) {
+        String res = "";
+        text = text.toUpperCase();
+        for (int i = 0, j = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c < 'A' || c > 'Z') continue;
+            res += (char)((c + key.charAt(j) - 2 * 'A') % 26 + 'A');
+            j = ++j % key.length();
+        }
+        return res;
+    }
+}
